399fc893ac6321012130a25a8351e433727f1da2
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-08 00:02:03 +0200

    displaying of list of users or variables beginner!#

--- Youtube/PedroTech/episode1/src/App.js
+++ Youtube/PedroTech/episode1/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
 function App() {
+  const name = <h1>Mwaka</h1>
+  const numbers = <p>1</p>
+  const email = <h2>email@getDefaultNormalizer.com</h2>
+  const user = (
+    <div>
+{name}
+    {numbers}
+    {email}
+    </div>
+    
+  );
   return (
     <div className="App">
-      Hello World
+      {user}
+      {user}
+      {user}
     </div>
   );
 }
