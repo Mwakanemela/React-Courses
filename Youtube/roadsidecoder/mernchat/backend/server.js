b0a40d6b13161d6bd40ac47024f43636b4fa5f46
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-11 14:59:35 +0200

    first server and express api

--- Youtube/roadsidecoder/mernchat/backend/server.js
+++ Youtube/roadsidecoder/mernchat/backend/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv")
+const { chats } = require("./data/data");
 
 
 //create instance of express
 const app = express()
-
+dotenv.config
 app.get("/", (req, res) => {
     res.send("API is running")
 })
 
-app.listen(9000, console.log("Server started on port 9000"))
+app.get("/api/chat", (req, res) => {
+    res.send(chats)
+})
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id == req.params.id);
+    res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 9000;  
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
