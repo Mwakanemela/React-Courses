a8e6fecc3a04172ab4d5e5c28121e8b2453edb21
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-11 09:40:13 +0200

    react css demo

--- Youtube/PedroTech/episode1/src/App.js
+++ Youtube/PedroTech/episode1/src/App.js
@@ -5,23 +5,10 @@ function App() {
   
   return (
     <div className="App">
-      <Job salary={90000} position="Senior SDE" company="Amazon"/>
-
-<Job salary={80000} position="Senior SDE" company="Amazon"/>
-<Job salary={90000} position="Senior SDE" company="Amazon"/>
+      <h1 className="name">Mwakanemela Kayange</h1>
 
     </div>
   );
-}
-const Job = (probs) => {
-  return(
-    <div>
-      <h1>{probs.salary}</h1>
-      <p>{probs.position}</p>
-      <p>{probs.company}</p>
-    </div>
-    
-  )
 }
 
 export default App;
