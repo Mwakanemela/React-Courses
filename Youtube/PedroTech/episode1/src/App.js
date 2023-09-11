84c897d6b4854867662f2ce778236f3c56118a88
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-11 09:48:52 +0200

    conditional statement demo in react

--- Youtube/PedroTech/episode1/src/App.js
+++ Youtube/PedroTech/episode1/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
   
-  
+  const area = 25;
   return (
     <div className="App">
       <h1 className="name">Mwakanemela Kayange</h1>
-
+      <div>{area == 25 ? <p>You live in area 25</p> : <p>You dont live in 25</p>}</div>
     </div>
   );
 }
