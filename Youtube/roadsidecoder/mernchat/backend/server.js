6b65ac08fe22915593718cfe6755edfa7acdfdbc
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-11 14:41:18 +0200

    Started an API

--- Youtube/roadsidecoder/mernchat/backend/server.js
+++ Youtube/roadsidecoder/mernchat/backend/server.js
@@ -4,4 +4,8 @@ const express = require("express");
 //create instance of express
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("API is running")
+})
+
 app.listen(9000, console.log("Server started on port 9000"))
