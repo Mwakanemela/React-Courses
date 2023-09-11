336bdc5e3d3685e325552067f78db5e46fb9a3b4
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-11 09:53:59 +0200

    styling color with tenary operators (inline)

--- Youtube/PedroTech/episode1/src/App.js
+++ Youtube/PedroTech/episode1/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 function App() {
   
   const area = 25;
+  const isGreen = true;
+
   return (
     <div className="App">
       <h1 className="name">Mwakanemela Kayange</h1>
       <div>{area == 25 ? <p>You live in area 25</p> : <p>You dont live in 25</p>}</div>
+      <h1 style={{color:isGreen ? "yellow" : "red"}} >Change color</h1>
     </div>
   );
 }
