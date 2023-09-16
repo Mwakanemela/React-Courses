81193dd63d94e7c131ee9bd01e2295610f682e37
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-16 08:46:19 +0200

    change text color

--- Youtube/PedroTech/episode1/src/App.js
+++ Youtube/PedroTech/episode1/src/App.js
@@ -6,10 +6,17 @@ import {useState} from "react"
    const toggleText = () => {
      setShowText(!showText)
    }
+
+   const [changeColor, setChangeColor] = useState("black")
+   const changeThemeColor = () => {
+     setChangeColor(changeColor === "black" ? "red" : "black" )
+   }
   return (
     <div className="App">
       <button onClick={toggleText}>Toggle Text</button>
+      <button onClick={changeThemeColor}>Change Theme Color</button>
       {showText && <h1>Change visibility</h1>}
+      <h1 style={{ color: changeColor }}>Change Theme Color</h1>
     </div>
   );
 }
