3205c4fc382dc71f4bc4dcb55d41565018d0c48a
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-16 09:13:25 +0200

    module 5: starting CRUD in react, todo list

--- Youtube/PedroTech/episode1/src/App.js
+++ Youtube/PedroTech/episode1/src/App.js
@@ -1,55 +1,9 @@
 import './App.css';
-import {useState} from "react"
  function App() {
   
-   var [showText, setShowText] = useState(true)
-   const toggleText = () => {
-     setShowText(!showText)
-   }
-
-   const [changeColor, setChangeColor] = useState("black")
-   const changeThemeColor = () => {
-     setChangeColor(changeColor === "black" ? "red" : "black" )
-   }
-
-   var [count, setCount] = useState(0)
-
-   const increaseNumber = () => {
-     setCount(count + 1)
-   }
-
-const decreaseNumber = () => {
-     setCount(count - 1)
-   }
-const setNumberToZero = () => {
-     setCount(count = 0)
-   }
-   
-
-   const [counterProgram, setCounterProgram] = useState(false)
-
-
-const toggleCounterProgram = (event) => {
-     setCounterProgram(!counterProgram )
-   }
   return (
     <div className="App">
-      <br />
-      <button onChange={toggleCounterProgram}>Counter Program</button>
-      <br/><br/><br/>
-      <button onClick={toggleText}>Toggle Text</button>
-      <button onClick={changeThemeColor}>Change Theme Color</button>
-      {showText && <h1>Change visibility</h1>}
-      <h1 style={{ color: changeColor }}>Change Theme Color</h1>
-
-
-
-      <button onClick={increaseNumber}>Increase</button>
-      <button onClick={decreaseNumber}>Decrease</button>
-      <button onClick={setNumberToZero}>Set to Zero</button>
-      {
-        count
-      }
+      Crud in React
     </div>
   );
 }
