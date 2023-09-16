76e0cca5422630e4c255ab67c0dc0580642cbc50
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-16 19:33:06 +0200

    react login and signup design using chackra ui

--- Youtube/roadsidecoder/mernchat/backend/data/models/messagModel.js
+++ Youtube/roadsidecoder/mernchat/backend/data/models/messagModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const messageSchema = mongoose.Schema(
+const messageModel = mongoose.Schema(
   {
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     content: { type: String, trim: true },
@@ -10,5 +10,5 @@ const messageSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const Message = mongoose.model("Message", messageSchema);
+const Message = mongoose.model("Message", messageModel);
 module.exports = Message;
