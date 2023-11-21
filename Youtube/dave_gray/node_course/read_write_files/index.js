aa537ddd246e1778230ee3cf2c7938461b43ffa7
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-11-21 23:44:33 +0200

    just to prove that node executes code asynchronouse(async and await)

--- Youtube/dave_gray/node_course/read_write_files/index.js
+++ Youtube/dave_gray/node_course/read_write_files/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 
-fs.readFile('./files/starte.txt', 'utf-8', (err, data) => {
+fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {
     if (err) throw err;
     console.log(data.toString())
 })
 
+console.log('Hello...')
 process.on('uncaughtException', err => {
     console.error(`uncaught error: ${err}`)
     process.exit(1)
