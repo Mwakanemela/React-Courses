8585bd75dd4ef3ee7be05a1403aec433b3191eb2
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-11-21 23:13:13 +0200

    logging some os commands - common js modules

---
+++ Youtube/dave_gray/node_course/part1/server.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+console.log(os.type())
+
+console.log(os.version())
+
+console.log(os.homedir())
+
+console.log(os.hostname())
+
+console.log(os.machine())
+
+console.log(os.getPriority())
+
+console.log(os.uptime())
+
+console.log(os.userInfo())
+
+
