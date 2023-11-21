bc1acad7e50ebb3e459f07c8a8874eecfa9fcab5
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-11-21 23:26:05 +0200

    way one of exporting modules

--- Youtube/dave_gray/node_course/part1/server.js
+++ Youtube/dave_gray/node_course/part1/server.js
@@ -1,4 +1,6 @@
-console.log(__dirname)
-console.log(__filename)
+const { add, sub, mul, divide } = require("./math")
 
-
+console.log(add(2, 3))
+console.log(sub(2, 3))
+console.log(mul(2, 3))
+console.log(divide(2, 3))
