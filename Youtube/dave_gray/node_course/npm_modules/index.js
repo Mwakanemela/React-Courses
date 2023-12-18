93125995bf4f460746376b4772f77fe2d87b55f1
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-12-19 00:02:35 +0200

    using uuid

--- Youtube/dave_gray/node_course/npm_modules/index.js
+++ Youtube/dave_gray/node_course/npm_modules/index.js
@@ -1,3 +1,6 @@
 const { format } = require('date-fns')
+const {v4: uuid} = require('uuid')//mport v4 as uuid
 
 console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
+console.log(uuid())
+console.log(uuid())
